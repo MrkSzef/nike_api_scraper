7d2eaf1bd06c03a2782ab58566fcf92885763040
MarekSzef <68927191+MrkSzef@users.noreply.github.com>
2024-09-16 21:55:01 +0200

    scraper v1

---
+++ commands/get_item.js
@@ -0,0 +1,73 @@
+const { SlashCommandBuilder, EmbedBuilder, Options, inlineCode, channelLink  } = require('discord.js');
+const { Data_Process } = require('../self-modules/scraper-main-functions');
+const fetch  =  require("node-fetch");
+const { region, language } = require('../config.json');
+const channels= ['008be467-6c78-4079-94f0-70e2d6cc4003',
+				'16134d36-74f2-11ea-bc55-00242ac13000',
+				'd9a5bc42-4b9c-4976-858a-f159cf99c647',
+				'010794e5-35fe-4e32-aaff-cd2c74f89d61',
+				'82a74ac1-c527-4470-b7b0-fb5f3ef3c2e2']
+
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('check')
+		.addStringOption(option => option
+			.setName('sku')
+			.setDescription("Provide Product SKU")
+			.setRequired(true))
+		.setDescription('😎'),
+	async execute(interaction) {
+		let number_of_requests = 0;
+		const sku = interaction.options.getString('sku');//16134d36-74f2-11ea-bc55-00242ac13000  d9a5bc42-4b9c-4976-858a-f159cf99c647
+		var link = "";
+		var response = "";
+		for(let index = 0; index < channels.length; index++){
+			link = `https://api.nike.com/product_feed/threads/v3/?filter=language(${language})&filter=marketplace(${region})&filter=channelId(${channels[index]})&filter=productInfo.merchProduct.styleColor(${sku})`
+			response = await fetch(link,{method: 'GET', headers:{'authority': 'api.nike.com','accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','accept-language': 'pl-PL,pl;q=0.9,en-US;q=0.8,en;q=0.7','cache-control': 'max-age=0','sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="101", "Google Chrome";v="101"','sec-ch-ua-mobile': '?0','sec-ch-ua-platform': '"Windows"','sec-fetch-dest': 'document','sec-fetch-mode': 'navigate','sec-fetch-site': 'none','sec-fetch-user': '?1','upgrade-insecure-requests': '1','user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36'}});
+			response = await response.json();
+			if(response['pages']['totalResources']>0){
+				number_of_requests = index;
+				break;
+			};
+		};
+		let DP = new Data_Process(response);
+		let Sizes = DP.get_sizes()
+
+		const Embed = new EmbedBuilder()
+		.setColor(0x307894)
+		.setTitle(DP.get_title())
+		.setAuthor({ name: DP.get_sku()})
+		.setDescription(
+			'```ansi\n'
+			+DP.get_status()
+			+DP.get_price()
+			+DP.get_full_region()
+			+"\n\n[1;2m---- Other Info ----[0m"
+			+DP.get_exclusive_acces_state()
+			+DP.get_drop_date()
+			+DP.get_drop_method()
+			+DP.get_IPO()
+			+DP.get_promotion_status()
+			+DP.get_size_range()
+			+DP.get_sizing_prefix()
+			+'```'
+		)
+		.setThumbnail('https://images.nike.com/is/image/DotCom/'+sku.replace('-','_')+'_A_PREM?fmt=png-alpha')
+		.setTimestamp()
+		.setFooter({ text: 'Made By marekszef'});
+		for(let i = 0; i < Sizes.length; i++){
+			Embed.addFields({ name: 'Sizes:', value: "```ansi\n"+Sizes[i].join("\n")+"```",inline:true});
+		};
+		if (toString(response["objects"][0]['publishedContent']['properties']['seo']['slug']).endsWith(response["objects"][0]['productInfo'][0]['merchProduct']['productRollup']['key'])){
+			Embed.addFields({name:"Links: ", value: `| [NIKE WEB](https://www.nike.com/${language}/t/sperka/${sku}) |`})
+		}
+		else{
+			Embed.addFields({name:"Links: ", value: `| [SNKRS WEB](https://www.nike.com/${language}/launch/r/${sku}) |`})
+		};
+		await interaction.reply({embeds: [Embed]});
+	},
+};
+
+
---
+++ self-modules/scraper-main-functions.js
@@ -0,0 +1,154 @@
+class Data_Process {
+    constructor(dane) {
+    this.dane = dane;
+		try{
+			this.start_date = new Date(dane["objects"][0]["productInfo"][0]["launchView"]["startEntryDate"]);
+			this.start_date.setHours(this.start_date.getHours()+1)
+			console.log(this.start_date);
+			this.stop_date = new Date(dane["objects"][0]["productInfo"][0]["launchView"]["stopEntryDate"]);
+		}
+		catch{}
+	}
+
+
+	add_zero(arg){
+		if(arg.toString().length === 1){
+			return "0".concat(arg.toString());
+		}
+		else{
+			return arg.toString();
+		}
+	};
+
+	
+	
+	
+    get_title(){
+		try{return this.dane["objects"][0]["productInfo"][0]["productContent"]["title"]+"\n"+this.dane["objects"][0]["productInfo"][0]["productContent"]["colorDescription"];}
+		catch{return this.dane["objects"][0]["productInfo"][0]["merchProduct"]["styleColor"];}
+	}
+	get_sku(){
+		try{return this.dane["objects"][0]["productInfo"][0]["merchProduct"]["styleColor"];}
+		catch{return "No Data";}
+	}
+	get_sizes(){
+		try{
+			let gtins = {};
+			let stock = [];
+			let LEVEL = ""
+			for (let index = 0; index < this.dane["objects"][0]["productInfo"][0]["availableGtins"].length; index++) {gtins[this.dane["objects"][0]["productInfo"][0]["availableGtins"][index]["gtin"]] = this.dane["objects"][0]["productInfo"][0]["availableGtins"][index]["level"];};
+			for (let index = 0; index < this.dane["objects"][0]["productInfo"][0]["skus"].length; index++) {
+				
+				try
+				{
+					LEVEL = gtins[this.dane["objects"][0]["productInfo"][0]["skus"][index]["gtin"]]
+					LEVEL=LEVEL
+						.replace("LOW","[2;31mLOW[0m")
+						.replace("MEDIUM","[2;33mMEDIUM[0m")
+						.replace("HIGH","[2;32mHIGH[0m")
+						.replace("OOS","[2;30mOOS[0m")
+					stock[index] = ["[1;2m"+this.dane["objects"][0]["productInfo"][0]["skus"][index]["countrySpecifications"][0]["localizedSize"]+"[0m",LEVEL].join(`${" ".repeat(5-(this.dane["objects"][0]["productInfo"][0]["skus"][index]["countrySpecifications"][0]["localizedSize"]).length)} | `);
+				}
+				catch (error)
+				{
+					stock[index] = ["[1;2m"+this.dane["objects"][0]["productInfo"][0]["skus"][index]["countrySpecifications"][0]["localizedSize"]+"[0m","[2;30mOOS[0m"].join(`${" ".repeat(5-(this.dane["objects"][0]["productInfo"][0]["skus"][index]["countrySpecifications"][0]["localizedSize"]).length)} | `);
+				};
+			}
+			if (stock.length < 10){return [stock];
+			}
+			else
+			{
+				if(stock.length%2 == 0)
+				{
+					return [stock.slice(0,(stock.length/2>>0)),stock.slice((stock.length/2>>0))];	
+				}
+				else
+				{
+					return [stock.slice(0,(stock.length/2>>0)+1),stock.slice((stock.length/2>>0)+1)];	
+				}	
+			}
+		}
+		catch  (error) {
+			console.log(error);
+		}
+	}
+	get_status(){
+		try{return "\n[1;2mStatus:[0m "+this.dane["objects"][0]["productInfo"][0]["merchProduct"]["status"]}
+		catch{return ""}
+	};
+	get_price(){
+		try{return "\n[1;2mPrice:[0m "+this.dane["objects"][0]["productInfo"][0]["merchPrice"]["currentPrice"]+" "+this.dane["objects"][0]["productInfo"][0]["merchPrice"]["currency"]}
+		catch{return ""}
+	};
+	get_full_region(){
+		try{return "\n[1;2mRegion:[0m "+this.dane["objects"][0]["marketplace"]+"-"+this.dane["objects"][0]["language"]}
+		catch{return ""}
+	};
+	get_IPO(){
+		try{return "\n[1;2mItems Per Order:[0m "+this.dane["objects"][0]["productInfo"][0]["merchProduct"]["quantityLimit"]}
+		catch{return ""}
+	};
+	get_promotion_status(){
+		try{
+		if (this.dane["objects"][0]['productInfo'][0]['merchPrice']['promoExclusions'].includes("TRUE")){
+			return '\n[1;2mPromo Codes:[0m Not Working ❌'
+		}
+		else if (this.dane["objects"][0]['productInfo'][0]['merchPrice']['promoInclusions'].includes("EverGreen Exclusions")){
+			return '\n[1;2mPromo Codes:[0m Not Working ❌'
+		}
+		else if (this.dane["objects"][0]['productInfo'][0]['merchPrice']['promoInclusions'].includes("AF1 AJ1 Exclusion")){
+			return '\n[1;2mPromo Codes:[0m Not Working ❌'
+		}
+		else{
+			return '\n[1;2mPromo Codes:[0m Working ✅'
+		}
+		}
+		catch (error){
+			console.log(error)
+			return ''
+		}
+	};
+	get_size_range(){
+		try{return "\n[1;2mSize Range:[0m "+this.dane["objects"][0]["productInfo"][0]["skus"][0]["countrySpecifications"][0]["localizedSize"]+" - "+this.dane["objects"][0]["productInfo"][0]["skus"].reverse()[0]["countrySpecifications"][0]["localizedSize"]
+	}
+		catch{
+			return "";
+		}
+	};
+	get_exclusive_acces_state(){
+		try{
+			return "\n[1;2mEA:[0m "+this.dane["objects"][0]['productInfo'][0]['merchProduct']['exclusiveAccess'];
+		}
+		catch{
+			return "";
+		}
+	};
+	get_sizing_prefix(){
+		try{
+			return "\n[1;2mSize Prefix:[0m "+this.dane["objects"][0]["productInfo"][0]["skus"][0]["countrySpecifications"][0]["localizedSizePrefix"]
+		}
+		catch{
+			return "";
+		}
+	};
+	get_drop_date(){
+		try{
+			return "\n[1;2mDrop Date:[0m "+this.start_date.getUTCFullYear()+"-"+this.add_zero(this.start_date.getUTCMonth())+"-"+this.add_zero(this.start_date.getUTCDate())+" "+this.add_zero(this.start_date.getUTCHours())+":"+this.add_zero(this.start_date.getUTCMinutes())+":"+this.add_zero(this.start_date.getUTCSeconds())+" UTC+0";
+		}
+		catch {
+			return "";
+		}
+	};
+	get_drop_method(){
+		try{
+			
+			return "\n[1;2mDrop Type:[0m "+this.dane["objects"][0]["productInfo"][0]["launchView"]["method"]
+		}
+		catch{
+			return "";
+		}
+	};
+}
+
+
+module.exports = {Data_Process};
