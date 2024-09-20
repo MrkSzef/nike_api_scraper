ae8892623ac62257e074568ec46e6086caf74c47
Marek <kabzinskimilosz@gmail.com>
2024-09-20 19:56:23 +0200

    Fixed errors with EA command

--- commands/get_early_access.js
+++ commands/get_early_access.js
@@ -15,25 +15,25 @@ module.exports = {
             .setTimestamp()
             .setFooter({ text: 'Made By marekszef'});
         for (let i = 0; i < channels.length; i++) {
-            const data = await fetch(
-                `https://api.nike.com/product_feed/threads/v3/?anchor=0&count=100
-                &filter=marketplace(${region})
-                &filter=language(${language})
-                &filter=channelId(${channels[i]})
-                &filter=exclusiveAccess(true)&filter=upcoming(true)
-                &sort=effectiveStartSellDateAsc`
+            
+            let data = await fetch(
+                `https://api.nike.com/product_feed/threads/v3/?anchor=0&count=50&filter=exclusiveAccess(true)&filter=upcoming(true)&filter=marketplace(${region})&filter=language(${language})&filter=channelId(${channels[i]})&sort=effectiveStartSellDateAsc`
             ).then(res => {return res.json()})
-
+            
             try {
-                data = Array.from(data['objects'])
-                data.forEach(element => {
+                data = Array.from(data['objects']).slice(0, 2);
+                await data.forEach(element => {
                         const DP = new Data_Process(element);
-                        Embed.addFields({ name: DP.get_title(), value: DP.get_sku(),inline:true});
+                        Embed.addFields({ name: DP.get_title(), value: `\`\`\`SKU: ${DP.get_sku()}\nSTOCK: ${DP.get_avg_stock()}\`\`\``,inline:false});
                         items_count++;
                 });
                 
-            } catch (error) {}
+            } catch (error) {
+                console.log(error);
+            }
         }
+        console.log(items_count,Embed);
+        
         if (items_count == 0){
             Embed.setFields({ name: "No Early Access Products", value: "Try Again Later 😎🔥",inline:true});
         }
