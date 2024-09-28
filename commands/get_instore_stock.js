527800307745b4f02fa1cec2e0699548359ce3bd
Marek <kabzinskimilosz@gmail.com>
2024-09-28 02:09:46 +0200

    basic instore scraping

--- commands/get_instore_stock.js
+++ commands/get_instore_stock.js
@@ -1,5 +1,8 @@
 const { SlashCommandBuilder, EmbedBuilder, Options, inlineCode, channelLink  } = require('discord.js');
-let { stores } = require('../data/stores.json');
+const { Data_Process } = require('../self-modules/scraper-main-functions');
+let { stores } = require('../data/stores.json')
+const { region, language, channels } = require('../config.json');
+
 
 stores = stores.slice(0,24);
 
@@ -19,6 +22,7 @@ module.exports = {
     async execute(interaction) {
         const sku = interaction.options.getString('sku');
         const store = interaction.options.getString('store');
+        let gtin_translation
 
         const response = await fetch(
             `https://api.nike.com/deliver/available_gtins/v3?filter=styleColor(${sku})&filter=storeId(${store})&filter=method(INSTORE)`,
@@ -29,14 +33,35 @@ module.exports = {
             interaction.reply({ content: 'No Such Product in This Store', ephemeral: true });
             return;
         }
+
+        
+        for (let i = 0; i < channels.length; i++) {
+            const response = await fetch(
+                `https://api.nike.com/product_feed/threads/v3/?filter=language(${language})&filter=marketplace(${region})&filter=channelId(${channels[i]})&filter=productInfo.merchProduct.styleColor(${sku})`,
+                {method: "GET"}
+            )
+            const data = await response.json();
+            //console.log(Array.from(data['objects']).length);
+            data['objects'].length
+            
+            
+            if (data['objects'].length > 0) {
+                DP  = new Data_Process(data['objects'][0]);
+                gtin_translation = DP.get_gtin_translation();
+                break;
+            }
+            if (i == channels.length) {
+                interaction.reply({ content: 'No Such Product in Nike Backend', ephemeral: true });
+                return;
+            }
+        }
+
         
         let data = (await response.json())['objects'];
         data = await Array.from(data).map(elm => {
-            return [elm['gtin'],elm['level']]
+            return [gtin_translation[elm['gtin']],elm['level']]
         })
-        console.log(data);
         
-        interaction.reply({ content: data.join("\n") }); 
+        interaction.reply({ content: data.sort((a, b) => a[0] - b[0]).join("\n") }); 
     }
 }
-
